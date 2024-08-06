8cdf3551cdf190c9a0cfd56b1b0e2ee22435f39f
hxj0x <2063818987@qq.com>
2024-08-06 19:38:07 +0800

    fix build error https://github.com/preactjs/preact/blob/main/jsx-runtime/src/index.js https://github.com/EasyWebApp/DOM-Renderer/blob/main/source/jsx-runtime.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,5 +3,7 @@ export {
   default as createElement,
   default as jsx,
   default as jsxDEV,
+  // Note that while exporting the JSX namespace is sufficient for type checking, the production runtime needs the jsx, jsxs, and Fragment exports at runtime, and the development runtime needs jsxDEV and Fragment. Ideally you add types for those too.
+  default as jsxs,
 } from "./createElement";
 export * from "./type";
